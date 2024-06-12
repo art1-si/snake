db6aa6964e68f6a98db6593d60f2cc2b564e0ba0
a.sikora <artsikora1@gmail.com>
2024-06-12 20:46:15 +0200

    added firebase support and and encrypt vault

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:snake/snake_game_app.dart';
 import 'package:snake/tools/logger/logger.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
   await runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
       runApp(const SnakeGameApp());
     },
     (error, stackTrace) => Logger.error(error.toString(), stackTrace),
